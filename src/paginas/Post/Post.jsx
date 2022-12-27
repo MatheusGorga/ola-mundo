c6fb5599c0de0ad3bd78db83225be9175a0bda0d
MatheusGorga <matheusgobar@gmail.com>
2022-12-27 20:03:55 -0300

    post nao encontrado

--- src/paginas/Post/Post.jsx
+++ src/paginas/Post/Post.jsx
@@ -1,6 +1,8 @@
+import PaginaPadrao from 'componentes/PaginaPadrao/PaginaPadrao';
 import PostModelo from 'componentes/PostModelo/PostModelo';
+import NaoEncontrado from 'paginas/NaoEncontrado/NaoEncontrado';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
-import { useParams } from 'react-router-dom';
+import { Route, Routes, useParams } from 'react-router-dom';
 import posts from '../../assets/json/posts.json';
 import styles from './post.module.css';
 function Post() {
@@ -9,17 +11,26 @@ function Post() {
     return post.id === Number(parametros.id);
   });
   if (!post) {
-    <h1>Post Nao encontrado</h1>;
+    return <NaoEncontrado />;
   }
   return (
-    <PostModelo
-      fotoCapa={`/assets/posts/${post.id}/capa.png`}
-      titulo={post.titulo}
-    >
-      <div className={styles.postMarkdownContainer}>
-        <ReactMarkdown>{post.texto}</ReactMarkdown>
-      </div>
-    </PostModelo>
+    <Routes>
+      <Route path='*' element={<PaginaPadrao />}>
+        <Route
+          index
+          element={
+            <PostModelo
+              fotoCapa={`/assets/posts/${post.id}/capa.png`}
+              titulo={post.titulo}
+            >
+              <div className={styles.postMarkdownContainer}>
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+              </div>
+            </PostModelo>
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
 
